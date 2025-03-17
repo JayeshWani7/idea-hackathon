32b532b246781ee72cf1b3c6dcf927be4419c7f3
JayeshWani7 <wanijayesh0@gmail.com>
2025-03-17 12:59:46 +0530

    Initial commit

---
+++ OneDrive/Desktop/idea-hackathon/aadhaar-scanner-frontend/next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default nextConfig;
